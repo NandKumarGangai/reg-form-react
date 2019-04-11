fa67eadc73823ad464db9c9687720c1fe2e98dfd
nandkumargangai <nandkumargangai1@outlook.com>
2019-04-11 23:21:16 +0530

    added react select

--- src/components/RegisterForm/index.js
+++ src/components/RegisterForm/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Select from 'react-select';
 
 const RegistrationForm = () => {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+  const props = {
+     
+    isSearchable: false,
+    // isClearable: false,
+  }
   return (
     <div>
       RegistrationForm
+      <Select 
+        options={options}
+        {...props}
+      />
     </div>
   )
 }
